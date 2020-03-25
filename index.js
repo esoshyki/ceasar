9d059acf42000db87ce141b3c43f1fb0e1643726
esoshyki <esoshyki@yandex.by>
2020-03-25 17:22:56 +0300

    feat: add ceasar cipher algorithm

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const { program } = require('commander');
- 
+const { pipeline } = require('stream');
+const fs = require('fs');
+const zlib = require('zlib');
+const clc = require('cli-color')
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+const checkChunkIsLetter = (char) => alphabet.includes(char.toUpperCase())
+
+const transform = (char, shiftKey) => {
+
+  const isUpperCase = str => str === str.toUpperCase();
+  return isUpperCase(char) ? 
+  String.fromCharCode(((char.charCodeAt(0) + shiftKey - 65) % 26) + 65) :
+  String.fromCharCode(((char.charCodeAt(0) + shiftKey - 97) % 26) + 97);
+}
+
 program
-  .option('-s, --shift', 'shifting')
+  .option('-s, --shift <value>', 'shifting')
   .option('-i, --input <file>', 'input file')
   .option('-o, --output <file>', 'output file')
   .option('-a, --action <action>', 'action')
   .parse(process.argv);
+
+ let result = '';
  
-if (program.shift) console.log(program.shift);
-if (program.input) console.log(program.input);
-if (program.output) console.log(program.output);
-if (program['action']) console.log(program['action']);
+const readStream = fs.createReadStream(program.input);
+readStream.on('readable', () => {
+  let chunk;
+  const shiftKey = program.shift ? parseInt(program.shift) : 0
+
+  while (null !== (chunk = readStream.read(1))) {
+    const char = chunk.toString('utf8');
+    if (checkChunkIsLetter(char)) {
+      const letter = transform(char, shiftKey)
+      result += letter
+    }
+  }
+})
+readStream.on('end', () => {
+  console.log(clc.yellow(`result : ${result}`))
+})
+
